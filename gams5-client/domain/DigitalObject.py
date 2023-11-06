be1c9b2a090dbd2062184fd9f1a560eea60d90de
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-11-06 10:23:40 +0100

    added very basic domain class for a digital object
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/47

--- gams5-client/domain/DigitalObject.py
+++ gams5-client/domain/DigitalObject.py
@@ -1,2 +1,27 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class DigitalObject:
+    """
+    Represents a digital object returned by the gams-api.
+    """
+
+    id: str
+    """
+    Id of a digital object e.g. demo1 or o:demo.1 
+    """
+
+    project: str
+    """
+    Project abbreviation = id of a GAMS project e.g. 'demo'
+    """
+
+    datastreams: List[str]
+    """
+    List of datastreams contained by the digital object, e.g. TEI_SOURCE and IMG.1.
+    """
+
+    # child_objects: List[str]
 
 
