d86b6d691ff3c923b1b88e6a7cd49a9fc845b6aa
Sebi <SebastianStoff@gmx.at>
2023-11-08 15:18:41 +0100

    mvoed logic to call the file to own main.py
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/47

--- gams5-client/GAMS5APIClient.py
+++ gams5-client/GAMS5APIClient.py
@@ -40,25 +40,5 @@ class GAMS5APIClient:
         """
         # TODO implement ...
 
-        
-if __name__ == "__main__":
-
-    log_file_path = f"{os.getcwd()}/logs/app.log"
-
-    # setup logging
-    logging.basicConfig(filename=log_file_path, encoding='utf-8', level=logging.DEBUG)
-
-    # example usage of the client
-    MY_PROJECT = "demo"
-    # TODO configuring - maybe for handling auth thats not a good idea? e.g. GET requesting should possible all the time VS state changing operations
-    # need to throw an error. --> maybe own configure_auth() method?
-    client = GAMS5APIClient("http://localhost:18085")
-    # configure authentication ins separate method.
-    client.configure_auth("admin", "admin")
-
-    found_objects = client.list_objects(MY_PROJECT)
-    print(found_objects) 
-
-    client.save_object("demo4", MY_PROJECT)
-
+    
     
