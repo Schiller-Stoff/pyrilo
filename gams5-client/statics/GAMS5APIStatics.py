81f15c7002153d8f658e00ea23591e6b152a4e18
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-11-26 12:27:17 +0100

    added local file paths to the python client
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/52

--- gams5-client/statics/GAMS5APIStatics.py
+++ gams5-client/statics/GAMS5APIStatics.py
@@ -1,13 +1,19 @@
 
+import os
 
 class GAMS5APIStatics:
     """
-    ...
+    This class contains all statics for the GAMS5 API, like the API root path, the demo project abbreviation, the demo user name, etc.
     """
 
     API_ROOT = "/api/v1"
     DEMO_PROJECT_ABBR = "demo"
     DEMO_USER = "admin"
+
+    # local paths for client to work
+    LOCAL_PROJECT_FILES_PATH = os.getcwd() + "/project"
+    LOCAL_SIP_FOLDERS_PATH = LOCAL_PROJECT_FILES_PATH + "/sips"
+    LOCAL_BAGIT_FILES_PATH = LOCAL_PROJECT_FILES_PATH + "/bagit"
 
     def __init__(self) -> None:
         pass
