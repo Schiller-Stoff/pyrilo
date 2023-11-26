31ccc6151c0400a66dd46973f070c30601054ea4
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-11-26 12:28:00 +0100

    Added demo logic to copy over project files to bagit structure
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/52

--- gams5-client/service/SIPBagitTransformerService.py
+++ gams5-client/service/SIPBagitTransformerService.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+from statics.GAMS5APIStatics import GAMS5APIStatics
 
 
 class SIPBagitTransformerService:
@@ -14,5 +17,24 @@ class SIPBagitTransformerService:
     def save(self, output_data):
         raise NotImplementedError()
 
-    def transform(self, input_data):
-        raise NotImplementedError()
+    def transform(self):
+        """
+        Transforms all project files to the bagit format.
+        """
+
+        # Get the path of the SIPs folder
+        sips_folder = GAMS5APIStatics.LOCAL_SIP_FOLDERS_PATH
+
+        # Get the path of the bags folder
+        bags_folder = GAMS5APIStatics.LOCAL_BAGIT_FILES_PATH
+
+        # Loop through the SIPs folder
+        for folder_name in os.listdir(sips_folder):
+            # Create the corresponding bags folder
+            bags_folder_path = os.path.join(bags_folder, folder_name)
+            os.makedirs(bags_folder_path, exist_ok=True)
+
+            # Copy contents from SIP folder to the data directory inside the generated bag
+            sip_folder_path = os.path.join(sips_folder, folder_name)
+            data_folder_path = os.path.join(bags_folder_path, "data")
+            shutil.copytree(sip_folder_path, data_folder_path)
