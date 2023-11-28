daa16b72d73bc75916ac5e96b3dfeb51822af408
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-11-28 11:05:02 +0100

    fixed display of error response in case of malformed solr filed
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/55

--- gams5-client/service/IntegrationService.py
+++ gams5-client/service/IntegrationService.py
@@ -27,9 +27,8 @@ class IntegrationService:
         r = request("POST", url, headers= make_headers(basic_auth=f'{self.auth[0]}:{self.auth[1]}') if self.auth else None, redirect=False, timeout=10)
 
         if r.status >= 400:
-            msg = f"Failed to integrate all objects for project {project_abbr}. POST request against {url}. Status: {r.status}."
+            msg = f"Failed to integrate all objects for project {project_abbr}. POST request against {url}. Status: {r.status}. Response: {r.json()}"
             logging.error(msg)
-            logging.error(f"Response: {r.json()}")
             raise ConnectionError(msg)
         else:
             logging.info(f"Successfully integrated all digital objects for project {project_abbr}.")
