707ac3a3ef3f98905cf275b93e4e8880a8d9dab5
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-11-30 10:52:08 +0100

    added xml namespaces to intern namespace declaration
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/56

--- gams5-client/service/content_model/GAMSXMLNamespaces.py
+++ gams5-client/service/content_model/GAMSXMLNamespaces.py
@@ -6,17 +6,20 @@ class GAMSXMLNamespaces:
     """
 
     GML_NAMESPACES: Final = {
+        "xml": "http://www.w3.org/XML/1998/namespace",
         "gml": "http://www.opengis.net/gml", 
         "gdas": "http://cm4f.org/gdas/"
     }
 
     TEI_NAMESPACES: Final = {
+        "xml": "http://www.w3.org/XML/1998/namespace",
         "": "http://www.tei-c.org/ns/1.0",
         "t": "http://www.tei-c.org/ns/1.0",
         "tei": "http://www.tei-c.org/ns/1.0",
     }
 
     SKOS_NAMESPACES: Final = {
+        "xml": "http://www.w3.org/XML/1998/namespace",
         "skos":"http://www.w3.org/2004/02/skos/core#",
         "dc11": "http://purl.org/dc/elements/1.1/",
         "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#" 
