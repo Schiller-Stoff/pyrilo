88536408335810c74d9a58ed6e7accc4e41f8344
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-01 10:53:07 +0100

    added missing addition of TEI_SOURCE.xml onto the sip.json
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/56

--- gams5-client/service/content_model/TEIService.py
+++ gams5-client/service/content_model/TEIService.py
@@ -84,6 +84,24 @@ class TEIService:
             contentFiles=[]
         )
 
+        # add TEI_SOURCE.xml as content file
+        object_metadata.contentFiles.append(
+            SIPFileMetadata(
+                # TODO tink about actual data assignment
+                title="TEI_SOURCE", 
+                dsid="TEI_SOURCE", 
+                bagpath="data/content/TEI_SOURCE.xml", 
+                mimetype="text/xml",
+                # TODO add actual size 
+                size=9999999,
+                # TODO add processing of missing statements! 
+                creator="TODO", 
+                description="TODO", 
+                rights="TODO", 
+                publisher="TODO", 
+            )
+        )
+
         # process images defined in the TEI document
         image_files = TEIService._handle_tei_images(xml_root)
         for image_file in image_files:
