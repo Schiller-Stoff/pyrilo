08841bbfff31dce1b5df468c015f3476c85dcbba
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-01 09:12:44 +0100

    added static class for the content model definitions
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/56

---
+++ gams5-client/service/content_model/ContentModels.py
@@ -0,0 +1,11 @@
+
+from typing import final
+
+class ContentModels:
+    """
+    Contains constants for the different content models.
+    """
+    TEI: final = "TEI"
+    LIDO: final = "LIDO"
+
+    
