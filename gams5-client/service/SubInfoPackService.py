b3ba4a39c8822cb0e6ee011b52fdc5e087aa74cd
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-05 13:38:32 +0100

    added walk_sip_folder function
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/63

--- gams5-client/service/SubInfoPackService.py
+++ gams5-client/service/SubInfoPackService.py
@@ -4,6 +4,8 @@ import tempfile
 from statics.GAMS5APIStatics import GAMS5APIStatics
 from urllib3 import encode_multipart_formdata, make_headers, request
 import zipfile
+import os
+from typing import Callable
 
 class SubInfoPackService:
   """
@@ -25,6 +27,15 @@ class SubInfoPackService:
     Requests all SIP files of a project from the gams-api.
     """
     raise NotImplementedError("Not implemented yet.")
-
-
-
+  
+  @staticmethod
+  def walk_sip_folder(lambda_func: Callable[[str], None]):
+    """
+    Walks through all SIP files and calls given function for each folder. 
+    :param lambda_func: function to call for each folder - gets the folderpath as parameter
+    """
+    for folder_name in os.listdir(GAMS5APIStatics.LOCAL_SIP_FOLDERS_PATH):
+      folder_path = os.path.join(GAMS5APIStatics.LOCAL_SIP_FOLDERS_PATH, folder_name)
+      if os.path.isdir(folder_path):
+        lambda_func(folder_path)
+    
