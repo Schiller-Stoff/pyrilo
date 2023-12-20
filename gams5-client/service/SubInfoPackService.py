e5b17388cc44debc4d3dad58d7d48dfc326cd255
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-20 11:17:15 +0100

    defining a * at walk_sip_dir will loop over all folders
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/68

--- gams5-client/service/SubInfoPackService.py
+++ gams5-client/service/SubInfoPackService.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from statics.GAMS5APIStatics import GAMS5APIStatics
-from urllib3 import encode_multipart_formdata, make_headers, request
 import os
 import xml.etree.ElementTree as ET
 from typing import Callable
@@ -10,13 +9,17 @@ class SubInfoPackService:
   """
   Handles submission information packages.
   """
+
+  PROJECT_ABBR: str | None = None
+
+  def __init__(self, project_abbr: str) -> None:
+    self.PROJECT_ABBR = project_abbr
   
-  @staticmethod
-  def walk_sip_folder(lambda_func: Callable[[str, str, str], None], pattern: str = None):
+  def walk_sip_folder(self, lambda_func: Callable[[str, str, str], None], pattern: str = None):
     """
     Walks through all SIP files and calls given function for each folder. Skips all folder with underscore in name / path. 
-    :param lambda_func: function to call for each folder - gets the folderpath as first parameter and the path to the source file as second parameter. Third parameter is the actually encountered folder pattern, like folder_demo --> "demo"
-    :param pattern: optional pattern to filter folders by name
+    :param lambda_func: function to call for each folder - gets the folderpath as first parameter and the path to the source file as second parameter. Third parameter is the actually encountered folder pattern, like folder_demo --> "demo". Fourth parameter is the actual foldername.
+    :param pattern: optional pattern to filter folders by name. By default filters all folders with underscore in name. If pattern is "*", all folders are processed.
     """
     for folder_name in os.listdir(GAMS5APIStatics.LOCAL_SIP_FOLDERS_PATH):
       folder_path = os.path.join(GAMS5APIStatics.LOCAL_SIP_FOLDERS_PATH, folder_name)
@@ -25,12 +28,20 @@ class SubInfoPackService:
         continue
 
       source_file_path = os.path.join(folder_path, GAMS5APIStatics.SIP_SOURCE_FILE_NAME)
+
+      # if the pattern is a star -> all folders will be processed
+      if pattern == "*":
+        encountered_folder_pattern = ""
+        if "_" in folder_name:
+          encountered_folder_pattern = folder_path.split("_",1)[1]
+        lambda_func(folder_path, source_file_path, encountered_folder_pattern, folder_name)
+
       # if pattern is not given, process all folders
       if pattern is None:
         # skip folders with underscore in name
         if "_" in folder_name: 
           continue
-        lambda_func(folder_path, source_file_path, "")
+        lambda_func(folder_path, source_file_path, "", folder_name)
 
       # if pattern is given, process only folders with matching name  
       else:
@@ -42,5 +53,5 @@ class SubInfoPackService:
         if folder_path.endswith("_" + pattern):
           # pass the actually encountered folder pattern to the lambda function
           encountered_folder_pattern = folder_path.split("_",1)[1]
-          lambda_func(folder_path, source_file_path, encountered_folder_pattern)
+          lambda_func(folder_path, source_file_path, encountered_folder_pattern, folder_name)
 
