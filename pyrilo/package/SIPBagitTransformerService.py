903bd1235fb70606e9a8ab9c465e0e5d4ea75773
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-01-15 12:56:27 +0100

    added some todo marks
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/86

--- pyrilo/package/SIPBagitTransformerService.py
+++ pyrilo/package/SIPBagitTransformerService.py
@@ -36,6 +36,7 @@ class SIPBagitTransformerService:
         with open(bagit_info_file_path, "w") as bagit_info_file:
             bagit_info_file.write("Bag-Software-Agent: Pyrilo\n")
             bagit_info_file.write("Bagging-Date: {}\n".format(datetime.datetime.now().strftime("%Y-%m-%d")))
+            # TODO add checksum here
             bagit_info_file.write("Payload Oxum: [SOME HASH]\n")
             # bagit_info_file.write("Contact-Email: example@example.com\n")
             # bagit_info_file.write("External-Description: Demo Bag\n")
@@ -50,6 +51,7 @@ class SIPBagitTransformerService:
         # Generate manifest-md5.txt file
         manifest_md5_file_path = os.path.join(bag_folder_path, "manifest-md5.txt")
         with open(manifest_md5_file_path, "w") as manifest_md5_file:
+            # TODO checksum 
             manifest_md5_file.write("data/content/TEI_SOURCE.xml HASH\n")
 
 
