950f63eee7d2870c5e066b5ddc717e4fa4a68f4d
Sebi <SebastianStoff@gmx.at>
2026-01-28 14:22:26 +0100

    pyrilo now uses the context of click instead of a global variable to pass around the pyrilo client
    https://zimlab.uni-graz.at/gams5/production/preprocessing/pyrilo/-/issues/3

--- src/pyrilo/cli.py
+++ src/pyrilo/cli.py
@@ -4,18 +4,21 @@ import click
 import logging
 from pyrilo.Pyrilo import Pyrilo
 
-#
-pyrilo: Pyrilo = Pyrilo("http://localhost:18085")
-
 @click.group()
 @click.option("--host", "-h", default="http://localhost:18085", help="The host of the GAMS5 instance")
 @click.option("--bag_root", "-r", default=None, help="Root folder path of the bagit files. Defaults to ./bags")
-def cli(host: str, bag_root: str):
+@click.pass_context
+def cli(ctx, host: str, bag_root: str):
     """
     Pyrilo is a command line tool for managing your GAMS5 project.
 
     """
     logging.basicConfig( encoding='utf-8', level=logging.INFO, force=True)
+
+    # ensure context exists and is a dictionary
+    ctx.ensure_object(dict)
+    client = Pyrilo(host)
+    ctx.obj['CLIENT'] = client
 
     if bag_root:
         # Resolve to absolute path immediately to avoid relative path confusion later
@@ -24,74 +27,92 @@ def cli(host: str, bag_root: str):
         # Default: The user is running this IN their project folder
         resolved_path = str(Path.cwd() / "bags")
 
-    pyrilo.configure(host, resolved_path)
-    pyrilo.login()
+    client.configure(host, resolved_path)
+    client.login()
 
 
 @cli.command(name="ingest", help="Ingest bags as digital objects and datastream for a project. Removes all digital objects and datastreams beforehand")
 @click.argument("project", required=True)
-def ingest(project: str):
-    pyrilo.create_project(project, "Demo project for testing purposes")
-    pyrilo.ingest_bags(project)
+@click.pass_context
+def ingest(ctx, project: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.create_project(project, "Demo project for testing purposes")
+    client.ingest_bags(project)
 
 
 @cli.command(name="create_project", help="Creates a project on GAMS")
 @click.argument("project", required=True)
 @click.argument("desc", required=False)
-def create_project(project: str, desc: str):
-    pyrilo.create_project(project, desc)
+@click.pass_context
+def create_project(ctx, project: str, desc: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.create_project(project, desc)
 
 @cli.command(name="update_project", help="Updates an existing project on GAMS")
 @click.argument("project", required=True)
 @click.argument("desc", required=False)
-def update_project(project: str, desc: str):
-    pyrilo.update_project(project, desc)
+@click.pass_context
+def update_project(ctx, project: str, desc: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.update_project(project, desc)
 
 @cli.command(name="delete_objects", help="Deletes all objects of a project on GAMS")
 @click.argument("project", required=True)
-def delete_objects(project: str):
-    pyrilo.delete_objects(project)
+@click.pass_context
+def delete_objects(ctx, project: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.delete_objects(project)
 
 @cli.command(name="delete_object", help="Deletes all objects of a project on GAMS")
 @click.argument("project", required=True)
 @click.argument("object_id", required=True)
-def delete_object(project:str, object_id: str):
-    pyrilo.delete_object(object_id, project)
+@click.pass_context
+def delete_object(ctx, project:str, object_id: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.delete_object(object_id, project)
 
 @cli.command(name="create_collection", help="Creates a collection of digital objects on the GAMS-API.")
 @click.argument("project", required=True)
 @click.argument("id", required=True)
 @click.argument("title", required=True)
 @click.argument("desc", required=False)
-def create_collection(project: str, id: str, title: str, desc: str):
+@click.pass_context
+def create_collection(ctx, project: str, id: str, title: str, desc: str):
     """
     Creates a GAMS collection of digital objects.
     A collection must have an "owning project".
     """
-    pyrilo.create_collection(project, id, title, desc)
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.create_collection(project, id, title, desc)
 
 @cli.command(name="delete_collection", help="Deletes a collection of digital objects on the GAMS-API.")
 @click.argument("project", required=True)
 @click.argument("id", required=True)
-def delete_collection(project: str, id: str):
+@click.pass_context
+def delete_collection(ctx, project: str, id: str):
     """
     Deletes a GAMS collection
     """
-    pyrilo.delete_collection(
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.delete_collection(
         project_abbr=project,
         collection_id=id
     )
 
 @cli.command(name="integrate", help="Integrates data of digital objects of a project with additional GAMS services like solr")
 @click.argument("project", required=True)
-def integrate(project: str):
-    pyrilo.setup_integration_services(project)
-    pyrilo.integrate_project_objects(project)
+@click.pass_context
+def integrate(ctx, project: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.setup_integration_services(project)
+    client.integrate_project_objects(project)
 
 @click.command(name="disintegrate", help="Disintegrates data of digital objects of a project from additional GAMS services like solr")
 @click.argument("project", required=True)
-def disintegrate(project: str):
-    pyrilo.disintegrate_project_objects(project)
+@click.pass_context
+def disintegrate(ctx, project: str):
+    client: Pyrilo = ctx.obj['CLIENT']
+    client.disintegrate_project_objects(project)
 
 @click.group()
 def sync():
@@ -101,20 +122,24 @@ def sync():
 @sync.command('custom_search', help="Handles synchronization with the custom_search service")
 @click.argument("project", required=True)
 @click.option("--remove", "-r", default=False, help="If set, removes all data from the custom_search service", is_flag=True)
-def base_search(project: str, remove: bool):
+@click.pass_context
+def base_search(ctx, project: str, remove: bool):
+    client: Pyrilo = ctx.obj['CLIENT']
     if remove:
-        pyrilo.disintegrate_project_objects_custom_search(project)
+        client.disintegrate_project_objects_custom_search(project)
     else:
-        pyrilo.integrate_project_objects_custom_search(project)
+        client.integrate_project_objects_custom_search(project)
 
 @sync.command('plexus_search', help="Handles synchronization with the plexus_search service")
 @click.argument("project", required=True)
 @click.option("--remove", "-r", default=False, help="If set, removes all data from the custom_search service", is_flag=True)
-def plexus_search(project: str, remove: bool):
+@click.pass_context
+def plexus_search(ctx, project: str, remove: bool):
+    client: Pyrilo = ctx.obj['CLIENT']
     if remove:
-        pyrilo.disintegrate_project_objects_plexus_search(project)
+        client.disintegrate_project_objects_plexus_search(project)
     else:
-        pyrilo.integrate_project_objects_plexus_search(project)
+        client.integrate_project_objects_plexus_search(project)
 
 
 cli.add_command(ingest)
