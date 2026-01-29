7859ee1f86535128dae5304af1752a293b8cb837
Sebi <SebastianStoff@gmx.at>
2026-01-29 12:53:34 +0100

    removed outdated static variables
    https://zimlab.uni-graz.at/gams5/production/preprocessing/pyrilo/-/issues/3

--- src/pyrilo/PyriloStatics.py
+++ src/pyrilo/PyriloStatics.py
@@ -6,22 +6,6 @@ class PyriloStatics:
 
     API_ROOT = "/api/v1"
     AUTH_ENDPOINT = API_ROOT + "/auth"
-    DEMO_PROJECT_ABBR = "demo"
-    DEMO_USER = "admin"
-
-    # source file name of sips
-    SIP_SOURCE_DATASTREAM_ID = "SOURCE"
-    SIP_SOURCE_FILE_NAME = SIP_SOURCE_DATASTREAM_ID + ".xml"
-    # Thumbnail
-    THUMBNAIL_DATASTREAM_ID = "THUMBNAIL"
-    THUMBNAIL_FILE_NAME = THUMBNAIL_DATASTREAM_ID + ".jpg"
-    THUMBNAIL_SIP_SOURCE_FILE_NAME = "1.JPG"
-    # search json
-    SIP_SEARCH_JSON_DATASTREAM_ID = "SEARCH_INDEX"
-    SIP_SEARCH_JSON_FILE_NAME = SIP_SEARCH_JSON_DATASTREAM_ID + ".json"
-    # integration api statics, e.g. type of digital object
-    # type field category for digital object type
-    INTEGRATION_API_OBJECT_TYPE = "digitalObject"
 
     def __init__(self) -> None:
         pass
